73f851efb24083defb7bf359c1f5b32381f09884
ShinBoReum <tlsqhfma@naver.com>
2020-11-14 23:39:20 +0900

    홈 웹 기능 구현

--- src/main/java/hello/helloSpring/controller/MemberController.java
+++ src/main/java/hello/helloSpring/controller/MemberController.java
@@ -1,15 +1,34 @@
 package hello.helloSpring.controller;
 
+import hello.helloSpring.domain.Member;
 import hello.helloSpring.service.Memberservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
     private final Memberservice memberservice;
 
     @Autowired
-    public MemberController(Memberservice memberservice){
+    public MemberController(Memberservice memberservice) {
         this.memberservice = memberservice;
     }
+
+    @GetMapping("/members/new")
+    public String creatForm() {
+        return "members/creatMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberservice.join(member);
+
+        return "redirect:/";
+    }
+
 }
