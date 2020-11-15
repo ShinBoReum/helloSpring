b8f18fe715d6757cd057c47890d16a04a4ee8ecf
ShinBoReum <tlsqhfma@naver.com>
2020-11-15 20:27:41 +0900

    순수 JDBC to springJDBCTemplate 으로 변경

--- src/main/java/hello/helloSpring/SpringConfig.java
+++ src/main/java/hello/helloSpring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.helloSpring;
 
 import hello.helloSpring.repository.JdbcMemberRepository;
+import hello.helloSpring.repository.JdbcTemplateMemberRepository;
 import hello.helloSpring.repository.MemberRepository;
 import hello.helloSpring.repository.MemoryMemberRepository;
 import hello.helloSpring.service.Memberservice;
@@ -27,7 +28,8 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
