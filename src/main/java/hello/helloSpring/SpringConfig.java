b2272f31c28b02a489c5e95850c02c82c7ad0837
ShinBoReum <tlsqhfma@naver.com>
2020-11-15 22:34:47 +0900

    스프링부트 JPA

--- src/main/java/hello/helloSpring/SpringConfig.java
+++ src/main/java/hello/helloSpring/SpringConfig.java
@@ -5,31 +5,29 @@ import hello.helloSpring.service.Memberservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableMBeanExport;
-
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em =em;
-    }
-    @Bean
-    public Memberservice memberservice() {
-        return new Memberservice(memberRepository());
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+    public Memberservice memberservice() {
+        return new Memberservice(memberRepository);
     }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        //return new MemoryMemberRepository();
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+////        return new JpaMemberRepository(em);
+//
+//    }
 
 }
